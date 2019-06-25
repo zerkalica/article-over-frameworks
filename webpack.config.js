34b7f0b84d239c8cedd3e39d5d07fac99d83cdf9
Sergey Yuferev <s.yuferev@qiwi.com>
2019-06-25 17:40:27 +0300

    build: docs

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const webpack = require('webpack')
-module.exports = {
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.ASSET_PATH': JSON.stringify(process.env.ASSET_PATH)
-    })
-  ]
-}
